e86a6316f377c49f71d286309318be237d50432c
Estela Martos <estelamgmm@gmail.com>
2026-02-26 23:23:20 +0100

    cambios sync

--- src/main/java/com/example/BackendApp/service/Servicio.java
+++ src/main/java/com/example/BackendApp/service/Servicio.java
@@ -3,6 +3,7 @@ package com.example.BackendApp.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class Servicio {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Async
     public void sendVerificationEmail(String to, String token) {
 
         String link = "http://localhost:8080/api/auth/verify?token=" + token;
